ed60a2eed57e17d377e80c43b286ce3ab404a326
Charles Seale <c.seale@icloud.com>
2020-12-02 03:41:45 -0500

    added minor comment regarding use of axios

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { BootstrapVue, BootstrapVueIcons, ToastPlugin } from 'bootstrap-vue';
 import './custom.scss';
 
 
-
+// Create http prototype for API calls - ex. this.$http.get(url, options)
 Vue.prototype.$http = axios;
 
 Vue.use(BootstrapVue);
