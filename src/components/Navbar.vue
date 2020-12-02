1e7e303df01d0b0844d7293104d1d0e5bcbcac71
Charles Seale <c.seale@icloud.com>
2020-12-02 10:54:08 -0500

    Added Login view and and code for the Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,25 +3,25 @@
         <div>
             <b-navbar toggleable="lg" type="dark" variant="banner">
                 <b-navbar-brand class="ml-2"><router-link to="/"><h4 class="m-0">Site Title</h4></router-link></b-navbar-brand>
-
                 <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
                 <b-collapse id="nav-collapse" is-nav>
-                <b-navbar-nav class="ml-auto">
+                    <b-navbar-nav class="ml-auto">
 
-                    <b-nav-item><router-link to="/">Home</router-link></b-nav-item>
-                    <b-nav-item><router-link to="/about">About</router-link></b-nav-item>
+                        <b-nav-item><router-link to="/">Home</router-link></b-nav-item>
+                        <b-nav-item><router-link to="/about">About</router-link></b-nav-item>
+                        <b-nav-item v-if="!user.user_id"><router-link to="/login">Login</router-link></b-nav-item>
+                        <b-nav-item-dropdown right v-if="user.user_id">
+                        
+                            <!-- Using 'button-content' slot -->
+                            <template v-slot:button-content>
+                                <router-link to="#">{{ user.name }}</router-link>
+                            </template>
 
-                    
-                    <b-nav-item-dropdown right>
-                    <!-- Using 'button-content' slot -->
-                    <template #button-content>
-                        <em>User</em>
-                    </template>
-                    <b-dropdown-item href="#">Profile</b-dropdown-item>
-                    <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-                    </b-nav-item-dropdown>
-                </b-navbar-nav>
+                            <b-dropdown-item><router-link class="profile-link" to="/profile">Profile</router-link></b-dropdown-item>
+                            <b-dropdown-item><span class="profile-link" @click="signOut()">Sign Out</span></b-dropdown-item>
+                        
+                        </b-nav-item-dropdown>
+                    </b-navbar-nav>
                 </b-collapse>
             </b-navbar>
         </div>
@@ -29,7 +29,17 @@
 </template>
 
 <script>
+    import { mapState } from 'vuex';
     export default {
+        name: 'menuBar',
+        computed: {
+            ...mapState([ 'user' ])
+        },
+        data() {
+            return {
+
+            }
+        }
         
     }
 </script>
@@ -41,6 +51,7 @@ a {
 
     :hover {
         text-decoration: none;
+        color: #FFF;
     }
 }
 em {
---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <b-row>
+            <b-container>
+                <h1>Page for login components</h1>
+            </b-container>
+        </b-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
